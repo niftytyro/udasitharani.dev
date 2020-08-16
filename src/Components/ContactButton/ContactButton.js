d30b608f258f4467622357dbe01c8d4d3a940941
udasitharani <udasitharani@gmail.com>
2020-08-16 12:13:08 +0530

    fixed next button mobile styling issue

--- src/Components/ContactButton/ContactButton.js
+++ src/Components/ContactButton/ContactButton.js
@@ -2,10 +2,10 @@ import React from "react";
 import DownArrow from "../../static/down-arrow.svg";
 import styles from "./ContactButton.module.css";
 
-function ContactButton({ text, handleClick }) {
+function ContactButton({ text, handleClick, classname = styles.text }) {
   return (
     <div onClick={handleClick} className={styles.button}>
-      <div className={styles.text}>{text}</div>
+      <div className={classname}>{text}</div>
       <img src={DownArrow} alt="Let's Talk" />
     </div>
   );
