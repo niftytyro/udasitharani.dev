d6e2331ff479f1171e480122a6156c45cae1fd82
udasitharani <udasitharani@gmail.com>
2020-09-02 23:56:03 +0530

    updated projects

--- src/Components/MyWork/MyWork.js
+++ src/Components/MyWork/MyWork.js
@@ -1,6 +1,8 @@
 import React, { useContext } from "react";
 import AppReferenceContext from "../../context/AppReferenceContext";
 import Covid19TrackerIMG from "../../static/project screenshots/Covid-19 Tracker.png";
+import BakBakChatAppIMG from "../../static/project screenshots/BakBak ChatApp.jpg";
+import MovieBookingAppIMG from "../../static/project screenshots/Movie Booking App.jpg";
 import ProjectTile from "../ProjectTile/ProjectTile";
 import styles from "./MyWork.module.css";
 
@@ -17,6 +19,20 @@ const MyWork = () => {
           projectURL="https://udasitharani.github.io/Covid-19-Tracker"
           projectDescription="Made with Reactjs and disease.sh API."
           projectTitle="Covid-19 Tracker"
+        />
+        <ProjectTile
+          projectID="bakbak-chatapp"
+          projectIMG={BakBakChatAppIMG}
+          projectURL=""
+          projectDescription="Made with Flutter and Firebase."
+          projectTitle="Chat App"
+        />
+        <ProjectTile
+          projectID="movie-booking-app"
+          projectIMG={MovieBookingAppIMG}
+          projectURL="../../static/project screenshots/movie_2.0_demo_mockup.mp4"
+          projectDescription="Made with Flutter and ❤️. Designed by Dimest C."
+          projectTitle="Movie Booking App"
         />
       </div>
     </div>
